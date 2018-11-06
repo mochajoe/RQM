f6bdb2a490b5f5d7781777b4fa8be97998c7de9e
Anthony Nguyen <anthony@Xueyans-MBP.charter.com>
2018-11-06 00:17:06 -0800

    set api response to state

--- src/Quote.js
+++ src/Quote.js
@@ -3,8 +3,8 @@ import './Quote.css';
 
 
 class Quote extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       quotes: [],
       quote: ''
@@ -14,14 +14,13 @@ class Quote extends Component {
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
       .then(results => results.json())
-      .then((data) => {
-        let quoteRay;
-        quoteRay = data.quotes; 
-      }
-    )
-      this.setState({
-        quotes: quoteRay
-      })
+      .then( data => {
+        let quotted =  data.quotes[9].quote
+        return this.setState({
+          quote:quotted
+        })  
+        })
+    
   }
 
   render() {
@@ -29,7 +28,7 @@ class Quote extends Component {
       <div className = "jumbotron">
         <div className = "container">
           <div id="quote-box">
-            <div id="text">dhgdfgdsfgdsfg</div>
+            <div id="text">{this.state.quote}</div>
             <div id="author">sdfsdfsdf</div>
             <button id="new-quote">New Quote</button>
             <a id="tweet-quote">tweet quote</a>
